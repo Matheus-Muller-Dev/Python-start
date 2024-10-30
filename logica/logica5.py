432936e5811ca766d07f0bfed2f01e2038e72630
Matheus Muller <matheusmiiller44@gmail.com>
2024-10-30 01:02:29 -0300

    deletando arquivos de logica

--- logica/logica5.py
+++
@@ -1,8 +0,0 @@
-preco = float(input("Qual o preco do produto: "))
-p_desconto = float(input("Informe o percentual do desconto: "))
-
-valor_desconto = (preco * p_desconto) / 100
-valor_apagar = preco - valor_desconto
-
-print(f"O valor a pagar é: {valor_apagar}")
-print(f"O valor do desconto é: {valor_desconto}")
