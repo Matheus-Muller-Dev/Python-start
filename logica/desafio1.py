69c74e01a2fc44c7dfaa4eb2148df16b6412299e
Matheus Muller <matheusmiiller44@gmail.com>
2024-10-30 01:55:11 -0300

    Desafio 1 adicionado

---
+++ logica/desafio1.py
@@ -0,0 +1,14 @@
+# Definindo a função apra calcular a área do retângulo
+# def calcular_area_retagulo (base, altura):
+#     area = base * altura
+#     return area
+
+
+
+
+base = float(input("Qual é a base: "))
+altura = float(input("Qual altura do retangulo? "))
+
+area = base * altura
+
+print(f"O total do triangulo é {area}")
